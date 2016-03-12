f6c6556d9730522ae2ce22eb28c76224090f3988
al1ensuu <462993145@qq.com>
2016-03-12 03:00:15 +0000

    Initial Commit

---
+++ PairSolitare/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PairSolitare
+//
+//  Created by Suu on 12/03/2016.
+//  Copyright © 2016 suu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
